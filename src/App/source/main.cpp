f5355876fab9684f517d0e74820585b59b581a65
StoneHappy <18017158729@163.com>
2022-08-13 14:43:51 +0800

    🎉 init: init project

--- src/App/source/main.cpp
+++ src/App/source/main.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
-#include "Core/test.h"
-int main(int argc, char *argv[])
+#define SOL_ALL_SAFETIES_ON 1
+#include <sol/sol.hpp>
+
+using namespace std;
+
+class Player
 {
-    test();
-    std::cout << "hello world!" << std::endl;
+public:
+    int AddHp(int add)
+    {
+        hp_ += add;
+        return hp_;
+    }
+
+    int hp_ = 0;
+};
+
+int main(int argc, char* argv[])
+{
+    sol::state sol_state;
+    sol_state.open_libraries(sol::lib::base);
+    sol::usertype<Player> usertype_player = sol_state.new_usertype<Player>("Player", sol::constructors<Player()>());
+    usertype_player["AddHp"] = &Player::AddHp;
+    usertype_player["hp_"] = &Player::hp_;
+
+    sol_state.script_file("./a.lua");
+
+    return 0;
 }
